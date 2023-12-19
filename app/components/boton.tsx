f31d939a2b9ab25e5517d006727526eb4f734ef3
miguelhuayhua <miguelhuayhuac2@gmail.com>
2023-12-19 19:15:06 -0400

    clase 1

---
+++ app/components/boton.tsx
@@ -0,0 +1,9 @@
+const Boton = () => {
+    return (<>
+        <div className="col-6">
+            <button className='btn btn-danger'>boton</button>
+        </div>
+    </>)
+}
+
+export default Boton;
